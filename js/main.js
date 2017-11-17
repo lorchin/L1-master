ad86fa78b9d08142a6917d6c51cfac5d74a4d5de
lorchin <32363233+lorchin@users.noreply.github.com>
2017-11-17 19:59:41 +0200

    popup on js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// var index = 0;
+
+// var elems = [];
+//     elems= document.getElementsByClassName ('slide');
+
+// console.log(elems);
+
+// function showPrev () {
+// 	console.log (index);
+// }
+// // 	elems.forEach(function(elem) {
+// //     elem.addEventListener("onmouseover", function(event) {
+// //       console.log(event.target);
+// //     });
+// // })
+
+// var profileLink = document.getElementsByClassName('profile-link')[0];
+// console.log(profileLink)
+var popup = document.getElementById("overlay");
+var profileLink = document.getElementById("profile");
+var close = document.getElementById("close");
+
+function showPopup() {
+	popup.classList.toggle("opened");
+	document.body.classList.toggle("nonScroll")
+}
+function closePopup() {
+	popup.classList.toggle("opened");
+	document.body.classList.toggle("nonScroll")
+}
+profileLink.addEventListener('click', showPopup);
+
+close.addEventListener('click', closePopup);
+
