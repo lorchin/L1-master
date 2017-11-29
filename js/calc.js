33d88ff178d5570c5a99ef4a71560416337e1219
lorchin <32363233+lorchin@users.noreply.github.com>
2017-11-29 21:54:51 +0200

    calc del add

--- js/calc.js
+++ js/calc.js
@@ -3,8 +3,8 @@ var DelItem_1 = document.getElementById('del_item_1');
 var RemoveItem_1 = document.getElementById('remove_1')
 
 
-var inputField = document.getElementById(count_item_1);
-var priceField = document.getElementById('price_item_1');
+var inputField_1 = document.getElementById('count_item_1');
+var priceField_1 = document.getElementById('price_item_1');
 
 
 AddItem_1.addEventListener("click", addItem_1);
@@ -19,25 +19,26 @@ function addItem_1() {
 
 	element_1.sum = element_1.quantity * element_1.price;
 
-//	inputField..... // выведи quantity
-	priceField.innerHTML = element_1.sum;
+	inputField_1.value = element_1.quantity;
+	priceField_1.innerHTML = element_1.sum;
 }
 
 
 function deleteItem_1() {
 	event.preventDefault();
-	element_1.quantity -= 1,
-	element_1.sumMin = element_1.price / element_1.quantity;
 
-	priceField.innerHTML = element_1.sumMin;
+if (element_1.quantity > 0) {
+	element_1.quantity -= 1;
+	inputField_1.value = element_1.quantity;
+	element_1.sum = element_1.sum - element_1.price;
+	priceField_1.innerHTML = element_1.sum;
+};
 }
-
 
 function removeItem_1() {
-
+	event.preventDefault();
+	document.RemoveItem_1.classList.toggle('hide');
 }
-
-
 
 
 var element_1 = {
@@ -57,39 +58,43 @@ var element_3 = {
 
 
 
-// var AddItem_2 = document.getElementById('add_item_2');
-// var DelItem_2 = document.getElementById('del_item_2');
-// var RemoveItem_2 = document.getElementById('remove_2')
+var AddItem_2 = document.getElementById('add_item_2');
+var DelItem_2 = document.getElementById('del_item_2');
+var RemoveItem_2 = document.getElementById('remove_2')
 
 
-// var inputField = document.getElementById(count_item_2);
-// var priceField = document.getElementById('price_item_2');
+var inputField_2 = document.getElementById(count_item_2);
+var priceField_2 = document.getElementById('price_item_2');
 
 
-// AddItem_2.addEventListener("click", addItem_2);
-// DelItem_2.addEventListener("click", deleteItem_2);
-// RemoveItem_2.addEventListener('click', removeItem_2)
+AddItem_2.addEventListener("click", addItem_2);
+DelItem_2.addEventListener("click", deleteItem_2);
+RemoveItem_2.addEventListener('click', removeItem_2)
 
 
 
-// function addItem_2() {
-// 	event.preventDefault();
-// 	element_2.quantity += 1,
-// 	element_2.sum = element_2.quantity * element_2.price;
+function addItem_2() {
+	event.preventDefault();
+	element_2.quantity += 1,
+	element_2.sum = element_2.quantity * element_2.price;
 
-// //	inputField.value // выведи quantity
-// 	priceField.innerHTML = element_2.sum;
-// }
-
-
-// function deleteItem_2() {
-// 	event.preventDefault();
-// 	element_2.quantity -= 1,
-// 	element_2.sum = element_2.quantity / element_2.price;
-// }
+//	inputField.value // выведи quantity
+	priceField_2.innerHTML = element_2.sum;
+}
 
 
-// function removeItem_2() {
+function deleteItem_2() {
+	event.preventDefault();
+	element_2.quantity -= 1,
+	// element_2.sum = element_2.sum / 35;
+	element_2.sumMin = element_2.quantity * element_2.price;
+	priceField_2.innerHTML = element_2.sum;
 
-// }
+
+}
+
+
+function removeItem_2() {
+
+}
 
--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-// var index = 0;
 
-// var elems = [];
-//     elems= document.getElementsByClassName ('slide');
-
-// console.log(elems);
-
-// function showPrev () {
-// 	console.log (index);
-// }
-// // 	elems.forEach(function(elem) {
-// //     elem.addEventListener("onmouseover", function(event) {
-// //       console.log(event.target);
-// //     });
-// // })
-
-// var profileLink = document.getElementsByClassName('profile-link')[0];
-// console.log(profileLink)
 var popup = document.getElementById("overlay");
 var profileLink = document.getElementById("profile");
 var close = document.getElementById("close");
