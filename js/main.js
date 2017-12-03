7c7b33195a311a55586d3947677ba7bb28bf45d3
lorchin <32363233+lorchin@users.noreply.github.com>
2017-12-04 01:54:02 +0200

    menu update

--- js/main.js
+++ js/main.js
@@ -17,24 +17,51 @@ close.addEventListener('click', closePopup);
 
 
 
-var slide1 = document.getElementById('slide1');
-var slide2 = document.getElementById('slide2');
-var slide3 = document.getElementById('slide3');
-var slide4 = document.getElementById('slide4');
-var slide5 = document.getElementById('slide5');
+// var slide1 = document.getElementById('slide1');
+// var slide2 = document.getElementById('slide2');
+// var slide3 = document.getElementById('slide3');
+// var slide4 = document.getElementById('slide4');
+// var slide5 = document.getElementById('slide5');
 
-var prevSlide = document.getElementById('prev');
-var nextSlide = document.getElementById('next');
+// var prevSlide = document.getElementById('prev');
+// var nextSlide = document.getElementById('next');
 
-function showPrev() {
-	slide5.classList.toggle('show');
+// function showPrev() {
+// 	slide5.classList.toggle('show');
+
+// }
+
+// function showNext() {
+// 	slide2.classList.toggle('show');	
+
+// }
+
+// prevSlide.addEventListener('click', showPrev);
+// nextSlide.addEventListener('click', showNext);
+
+
+
+
+
+
+
+var slides = document.querySelectorAll('#s1 .wrap');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,4000);
+function nextSlide() {
+slides[currentSlide].className = 'wrap';
+currentSlide = (currentSlide+1)%slides.length;
+slides[currentSlide].className = 'wrap show';
 }
 
-function showNext() {
-	slide2.classList.toggle('show');	
-}
-prevSlide.addEventListener('click', showPrev);
-nextSlide.addEventListener('click', showNext);
+
+
+
+
+
+
+
+
 
 
 
@@ -46,7 +73,6 @@ function showMobileMenu() {
 	document.body.classList.toggle('nonScroll')
 }
 pie.addEventListener('click',showMobileMenu)
-
 
 
 
